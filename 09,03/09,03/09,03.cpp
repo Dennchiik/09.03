5365458fad2f7d9960a9fc1af8b9008649723c24
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2024-04-18 23:41:25 +0300

    Add files via upload

---
+++ 09,03/09,03/09,03.cpp
@@ -0,0 +1,62 @@
+﻿//первое задание 
+/*#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream inputFile("input.txt");
+    std::ofstream outputFile("output.txt");
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Unable to open input file." << std::endl;
+        return 1;
+    }
+
+    if (!outputFile.is_open()) {
+        std::cerr << "Unable to create output file." << std::endl;
+        return 1;
+    }
+
+    std::string word;
+    while (inputFile >> word) {
+        if (word.length() >= 7) {
+            outputFile << word << " ";
+        }
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
+*/
+//второе задание 
+/*#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream inputFile("input.txt");
+    std::ofstream outputFile("output.txt");
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Unable to open input file." << std::endl;
+        return 1;
+    }
+
+    if (!outputFile.is_open()) {
+        std::cerr << "Unable to create output file." << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        outputFile << line << std::endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
+*/
